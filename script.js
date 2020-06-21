6aef6558f5acf4b11cae7a2fe2aea649c66280a3
Carlo <pbjunkiep3@yahoo.com>
2020-06-21 18:44:27 +1000

    18:44,21:06

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var update = function () {
-    document.getElementById("currentDay")
-        .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-}
+  document.getElementById("currentDay").innerHTML = moment().format(
+    "MMMM Do YYYY, h:mm:ss a"
+  );
+};
 setInterval(update, 1000);
 
 $(document).ready(function () {
-    //check the time change colors of rows
-    $("#button9").click(function () {
-        let userInput = $("#input9").val();
-        console.log(userInput);
-    })
-})
+  //check the time change colors of rows
+  $("saveBtn").click(function () {
+    let userInput = $(this).val();
+    console.log(userInput);
+  });
+});
 
+// const time = ["09","10","11","12","13","14","15","16","17"];
+
+// for(let x = 0; x < time.length; x++){
+
+//     const newDiv = $("<div>");
+//     newDiv.text(time[x]);
+
+// }
